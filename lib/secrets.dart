e2418b454f7d61ac29c89c6e047b1316148e79d9
shafiul-haque-johny <mhaque171319@bscse.uiu.ac.bd>
2023-10-12 16:21:51 +0600

    check on physical device

--- lib/secrets.dart
+++ lib/secrets.dart
@@ -1,4 +1,4 @@
 /// secret api key , generate by yourself from openAI!
 /// I have Generated it but not sharing it.
 
-const openAIAPIKEY = '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';
+const openAIAPIKEY = '';
